bd42ccb3beac75ff952d728b3ee24c9ec19c791b
EKfire <66260345+EKfire@users.noreply.github.com>
2023-12-11 15:20:14 -0500

    Add files via upload

---
+++ Portfolio-Stock-System/src/LogRegPage.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class LogRegPage {
+    //Instance variables
+    Scanner input = new Scanner(System.in);
+    String regOrLogInput;
+    String name, username, password, email;
+    
+    //Methods
+    public void run() {
+        while(true) {
+            System.out.println("Do you wish to (l)ogin or (r)egister");
+            regOrLogInput = input.nextLine();
+            
+            if(regOrLogInput.equalsIgnoreCase("l")) {
+                System.out.println("Enter username:\n");
+                username = input.nextLine();
+                System.out.println("Enter password:\n");
+                password = input.nextLine();
+                LogIn login = new LogIn(username, password);
+                login.runLogReg();
+            }else if(regOrLogInput.equalsIgnoreCase("r")) {
+                System.out.println("Enter name:\n");
+                name = input.nextLine();
+                System.out.println("Enter password:\n");
+                password = input.nextLine();
+                System.out.println("Enter email:\n");
+                email = input.nextLine();
+                Register register = new Register(name, password, email);
+                register.runLogReg();
+            }
+            System.out.println("Do you wish to shutdown the system, (y)es or (n)o?");
+            String inputText = input.nextLine();
+            if(inputText.contentEquals("y")) {
+                break;
+            }
+        }
+    }
+}
+
