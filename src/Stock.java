7e244079f2127798e180bb4a8429e10aef5fb37e
EKfire <66260345+EKfire@users.noreply.github.com>
2023-12-11 16:20:04 -0500

    hi

---
+++ src/Stock.java
@@ -0,0 +1,37 @@
+public class Stock {
+    //Attributes
+    private String symbol;
+    private double price;
+    private double share;
+    private double buyInPrice;
+
+    //Constructor
+    public Stock(String symbol, double price, double share, double buyInPrice) {
+        this.symbol = symbol;
+        this.price = price;
+        this.share = share;
+        this.buyInPrice = buyInPrice;
+    }
+
+    //Access Function
+    public String getSymbol(){
+        return this.symbol;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public double getShare(){
+        return this.share;
+    }
+    public double getBuyInPrice(){
+        return this.buyInPrice;
+    }
+
+    //Mutator Function
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void setShare(double share){
+        this.share = share;
+    }
+}
---
+++ src/SysClient.java
@@ -0,0 +1,6 @@
+public class SysClient {
+    public SysClient(){}
+    public void run(){
+        
+    }
+}
